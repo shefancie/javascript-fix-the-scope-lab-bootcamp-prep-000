54c8e64176acee9ac0b589a416fa575df9795f36
Ayana ! <ayana.singhateh@gmail.com>
2018-05-14 19:52:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,7 @@
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
-}
-
+ 
 function add2(n) {
   return n + two
 
